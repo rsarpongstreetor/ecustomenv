dd1a8ca747e6523935f2b6dde8e05d6752fdc453
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-02-10 23:47:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
    setup(
        name='customenv',
        version='0.1.0',  # Choose your version number
-       packages=find_packages(where='/content/BenchMARL/benchmarl/environments/'),
+       packages=find_packages(),
        install_requires=[
+             'BenchMARL'
            # Add any dependencies your package needs
            # Example: 'torch>=1.10.0'
        ],
