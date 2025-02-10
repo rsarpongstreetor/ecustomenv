728ba1efbcd8a7e76d3fdfd02e3c4aec03646913
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-02-11 00:48:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
        name='customenv',
        version='0.1.0',  # Choose your version number
-       packages=find_packages(),
+       packages=find_packages(content/BenchMARL/benchmarl/environments),
        install_requires=[
              'BenchMARL','torchrl', 'tensordict', 'torch_geometric', 'pyyaml'
            # Add any dependencies your package needs
